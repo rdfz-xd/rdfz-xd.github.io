4cae386e8ac922827a79cc8eaed80afa426e4b15
rdfz-xd <xingdongakioi@hotmail.com>
2025-11-10 19:39:28 -0500

    Update site 2025-11-10 19:39:28

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ npx quartz build
 rm -rf docs
 mv public docs
 git add -A
-git commit -m "Update site"
+git commit -m "Update site $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
 echo "Site updated! Visit https://rdfz-xd.github.io"
